a8128e23e8106e7d7751115824ad0e496928f159
DavidEdwardDarden <davidedarden83@gmail.com>
2021-07-18 19:34:35 -0500

    first commit

---
+++ Ready/01_Db_Create.sql
@@ -0,0 +1,48 @@
+﻿USE [master]
+GO
+
+IF db_id('Ready') IS NOT NULL
+BEGIN
+  ALTER DATABASE [Ready] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE [Ready]
+END
+GO
+
+CREATE DATABASE [Ready]
+GO
+
+USE [Ready]
+GO
+
+---------------------------------------------------------------------------
+
+
+
+
+CREATE TABLE [UserProfile] (
+  [id] int PRIMARY KEY,
+  [fierbaseUserId] nvarchar(255),
+  [createDateTime] datetime,
+  [fname] nvarchar(255),
+  [lname] nvarchar(255),
+  [email] nvarchar(255),
+  [username] nvarchar(255),
+  [password] nvarchar(255)
+)
+GO
+
+CREATE TABLE [Questions] (
+  [id] int PRIMARY KEY,
+  [question_Number] int,
+  [my_Question] bit,
+  [userId] int,
+  [question] nvarchar(255),
+  [answer] nvarchar(255),
+  [learned] bit,
+  [isDeleted] bit,
+  [created] datetime
+)
+GO
+
+ALTER TABLE [questions] ADD FOREIGN KEY ([userId]) REFERENCES [UserProfile] ([id])
+GO
