45d2e5e1fbbec8337110cd9e51774e19483792dd
DavidEdwardDarden <davidedarden83@gmail.com>
2021-07-21 09:35:57 -0500

    removed soft deletes from db AND sql file

--- SQL/01_Db_Create.sql
+++ SQL/01_Db_Create.sql
@@ -22,8 +22,7 @@ CREATE TABLE [UserProfile] (
   [FirstName] varchar(255),
   [LastName] varchar(255),
   [Email] varchar(255),
-  [UserName] varchar(255),
-  [IsDeleted] bit
+  [UserName] varchar(255)
 )
 
 CREATE TABLE [Questions] (
@@ -33,16 +32,13 @@ CREATE TABLE [Questions] (
   [QuestionContent] varchar(255),
   [AnswerContent] varchar(255),
   [Learned] bit,
-  [IsDeleted] bit,
   [CreateDateTime] datetime
 )
 
 CREATE TABLE [Category] (
   [Id] int PRIMARY KEY,
-  [Title] varchar(255),
-  [IsDeleted] bit
-
-
+  [Title] varchar(255)
+  
 CONSTRAINT [FK_Questions_UserProfile] FOREIGN KEY ([UserProfileId]) REFERENCES [UserProfile] ([Id]),
 CONSTRAINT [FK_PostReaction_Reaction] FOREIGN KEY ([ReactionId]) REFERENCES [Reaction] ([Id])
 
