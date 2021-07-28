8277548fcfd01293d6b05eec0b8be740c0210d2c
DavidEdwardDarden <davidedarden83@gmail.com>
2021-07-28 14:09:07 -0500

    updated going between question and answer

--- Ready/Client/src/components/Quiz/Quiz.js
+++ Ready/Client/src/components/Quiz/Quiz.js
@@ -1,47 +1,40 @@
 import { React, useEffect, useState } from "react";
-// import { getSubscribedPosts } from "../modules/subscriptManager.js";
-// import  Post from "./posts/PostListCard.js"
-// import thing from "./images/ReadyLogo.jpg";
 import thing from "../images/ReadyLogo.jpg";
 import thing2 from "../images/crown.png";
 import thing3 from "../images/pattern2.png";
 import "../Home.css";
-// import useDropdownMenu from "react-accessible-dropdown-menu-hook";
 import { useHistory, useParams } from "react-router-dom";
 import { getAllQuestionsByCategoryId } from "../../modules/quizManager";
 
 const Quiz = () => {
-  let y = -1;
   const { CategoryId } = useParams();
-  const [question, setQuestion] = useState({});
   const [questionList, setQuestionList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
+  const [showAnswer, setShowAnswer] = useState(false);
+  let [x, setX] = useState(0);
   const history = useHistory();
 
-  const getQList = () =>{
-
-    // console.log("Look here")
+  //!  QUESTION ARRAY  ---------------------------------------
+  const getQList = () => {
     getAllQuestionsByCategoryId(CategoryId).then((results) => {
-        setQuestionList(results)
+      setQuestionList(results);
 
-        // console.log(results,"Look here")
-  })}
-  
-  
-  //The Effect Hook ("useEffect") lets you perform side effects in function components:
-  //fetch list of all categories for dropdown
+    });
+  };
+
+  //!  SET STATE  ---------------------------------------
   useEffect(() => {
-   getQList()
+    getQList();
   }, []);
 
+  //!  click ->  EXIT BUTTON ---------------------------------------
   const handleClickEvent = (e) => {
     e.preventDefault();
     setIsLoading(true);
-
     history.push(`/`);
   };
 
+  //!  click ->  GOT IT BUTTON ---------------------------------------
   const handleClickEventGotIt = (e) => {
     e.preventDefault();
     setIsLoading(true);
@@ -55,23 +48,34 @@ const Quiz = () => {
     handleQuestionClickEvent(e);
   };
 
-  const handleClickEventNotYet = (e) => {
-    e.preventDefault();
-    setIsLoading(true);
 
-    //ask a new question
-    handleQuestionClickEvent(e);
+//!  UPDATE FUNCTION (CALL INSIDE CLICK EVENTS)---------------------------------------
+  const updateQuestion=() =>{
+    setX(x+1)
+    setShowAnswer(false)
+  }
+
+
+//!  click ->  NOT YET BUTTON ---------------------------------------
+    const handleClickEventNotYet = (e) => {
+//ALREADY ON QUESTION 1 BEFORE YOU CLICK THE QUESTION
+let count=1;
+count= count + 1;
+if(count < questionList.count)
+updateQuestion()
+
   };
+  
 
+//!  click ->  ON THE CARD  ---------------------------------------
   const handleQuestionClickEvent = (e) => {
-    e.preventDefault();
-    setIsLoading(true);
 
-    y++;
-
-    return <div>`${questionList[y].AnswerContent}`</div>;
+    setShowAnswer(!showAnswer)
+   
   };
 
+
+  //!  WELCOME TO THE DOM!  ---------------------------------------
   return (
     <>
       <div className="orange">
@@ -82,12 +86,14 @@ const Quiz = () => {
         <button onClick={handleClickEvent} className="centermeexitbutton">
           Exit
         </button>
+
         <button
           onClick={handleClickEventGotIt}
           className="centermebuttonproceed1"
         >
           GOT IT
         </button>
+
         <button
           onClick={handleClickEventNotYet}
           className="centermebuttonproceed2"
@@ -96,8 +102,10 @@ const Quiz = () => {
         </button>
 
         <section onClick={handleQuestionClickEvent} className="questioncard">
-          {/* functions are not valid as react children */}
-          <div> {<div>{questionList[0]?.questionContent}</div>} </div>
+        
+            <div> {!showAnswer &&<div>{questionList[x]?.questionContent}</div>} </div>
+            <div> {showAnswer &&<div>{questionList[x]?.answerContent}</div>} </div>
+
         </section>
       </div>
     </>
